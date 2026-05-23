3b9d0334e6134653b2da94a40a82c54237b8e7d2
utkarsh srivastava <utkarsh.srivastava3@globallogic.com>
2026-05-24 00:38:41 +0530

    fixed for windows

--- backend/db.js
+++ backend/db.js
@@ -11,6 +11,7 @@ async function initDb(retryCount = 0) {
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
+      port: process.env.DB_PORT,
     });
     connection.on("error", async (err) => {
       console.error("DB connection error:", err);
